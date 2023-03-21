dae60b4d05fbe6fce65ffd282c1c682bd0cd7ace
Alejandro Alatorre <alejandro.c.alatorre@gmail.com>
2023-03-21 13:14:06 -0700

    FINITO

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+
+            Console.Write("We are going to convert your celsius temperature" +
+                " to fahrenheit.\nPlease enter your temperature: ");
+            var userInput = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Your temperature in fahreheit is {TempConverter.CelsiusToFahrenheit(userInput)}°");
+
+            Console.WriteLine();
+
+            Console.Write("We are now going to convert your fahrenheit temperature" +
+                " to celsius.\nPlease enter your temperature: ");
+            var userInput2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Your temperature in celsius is {TempConverter.FahrenheitToCelsius(userInput2)}°");
+
+
         }
+
+
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    static class TempConverter
+    {
+        static TempConverter()
+        {
+            
+        }
+
+        public static double CelsiusToFahrenheit(double temp)
+        {
+            
+            
+            double fahrenheit = Math.Round((temp * 1.8) + 32);
+            return fahrenheit;
+            
+
+
+        }
+
+        public static double FahrenheitToCelsius(double temp)
+        {
+            double celsius =Math.Round((temp - 32) * .5556);
+            return celsius;
+        }
+        
+
+
+
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
